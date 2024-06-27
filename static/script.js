03fc6b82ecfbe111092c85b0311cf2d9d277b861
Sravya Prakasan <98803329+SravyaPrakasan@users.noreply.github.com>
2024-06-28 01:34:09 +0530

    Add files via upload
    parent css

---
+++ static/script.js
@@ -0,0 +1,15 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting the traditional way
+
+    const userType = document.getElementById('user-type').value;
+
+    if (userType === 'student') {
+        window.location.href = 'student_dashboard.html';
+    } else if (userType === 'teacher') {
+        window.location.href = 'teacher_dashboard.html';
+    } else if (userType === 'parent') {
+        window.location.href = 'pindex.html';
+    } else {
+        alert('Please select a user type');
+    }
+});
